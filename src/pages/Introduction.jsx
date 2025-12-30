d92056aae81a3a7494a4e837160285361c4f52c9
Leon Cantillo <leonjob13@gmail.com>
2025-12-30 14:01:32 -0500

    [UPDATE] Sections padding.

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Layout from "../components/Layout";
+import './../styles/introduction.scss';
 import './../styles/section_hero.scss';
 
 const Introduction = () => {
