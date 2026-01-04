92441092bb9ec2223aff14fccaaa911d121a4860
Leon Cantillo <leonjob13@gmail.com>
2026-01-03 20:41:20 -0500

    [ADD] Skills information for the portfolio.

---
+++ src/assets/skills.data.js
@@ -0,0 +1,42 @@
+import * as SkillLogos from './DevelopSkillLogos';
+import * as ToolLogos from './DevelopToolLogos';
+
+export const skillsData = [
+  {
+    title: "Servidor",
+    modifier: "server",
+    skills: [
+      { icon: SkillLogos.C, label: "C" },
+      { icon: SkillLogos.Cpp, label: "C++" },
+      { icon: SkillLogos.Java, label: "Java" },
+      { icon: SkillLogos.Kotlin, label: "Kotlin" },
+      { icon: SkillLogos.Spring, label: "Spring" },
+      { icon: SkillLogos.TypeScript, label: "TypeScript" },
+      { icon: SkillLogos.Expressjs, label: "ExpressJs" },
+      { icon: SkillLogos.MySQL, label: "MySQL" },
+      { icon: SkillLogos.PostgreSQL, label: "PostgreSQL" },
+      { icon: SkillLogos.Php, label: "PHP" },
+    ],
+  },
+  {
+    title: "Cliente",
+    modifier: "client",
+    skills: [
+      { icon: SkillLogos.JavaScript, label: "JavaScript" },
+      { icon: SkillLogos.React, label: "React" },
+      { icon: SkillLogos.Threejs, label: "ThreeJs" },
+    ],
+  },
+  {
+    title: "Herramientas",
+    modifier: "tools",
+    skills: [
+      { icon: ToolLogos.Git, label: "Git" },
+      { icon: ToolLogos.Docker, label: "Docker" },
+      { icon: ToolLogos.Postman, label: "Postman" },
+      { icon: ToolLogos.Nodejs, label: "NodeJs" },
+      { icon: ToolLogos.Vite, label: "Vite" },
+      { icon: ToolLogos.WordPress, label: "Wordpress" },
+    ],
+  },
+];
