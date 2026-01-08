e381f56389c3ae15d0bee7699206220dece3ea5b
Leon Cantillo <leonjob13@gmail.com>
2026-01-08 15:37:26 -0500

    [UPDATE] Tech logos. Single document. Generalizing.

--- src/portfolio-data/skills.data.js
+++ src/portfolio-data/skills.data.js
@@ -1,42 +1,41 @@
-import * as SkillLogos from '../assets/DevelopSkillLogos';
-import * as ToolLogos from '../assets/DevelopToolLogos';
+import * as TechLogos from '../assets/TechLogos';
 
 export const skillsData = [
   {
     title: "Servidor",
     modifier: "server",
     skills: [
-      { icon: SkillLogos.C, label: "C" },
-      { icon: SkillLogos.Cpp, label: "C++" },
-      { icon: SkillLogos.Java, label: "Java" },
-      { icon: SkillLogos.Kotlin, label: "Kotlin" },
-      { icon: SkillLogos.Spring, label: "Spring" },
-      { icon: SkillLogos.TypeScript, label: "TypeScript" },
-      { icon: SkillLogos.Expressjs, label: "ExpressJs" },
-      { icon: SkillLogos.MySQL, label: "MySQL" },
-      { icon: SkillLogos.PostgreSQL, label: "PostgreSQL" },
-      { icon: SkillLogos.Php, label: "PHP" },
+      { icon: TechLogos.C, label: "C" },
+      { icon: TechLogos.Cpp, label: "C++" },
+      { icon: TechLogos.Java, label: "Java" },
+      { icon: TechLogos.Kotlin, label: "Kotlin" },
+      { icon: TechLogos.Spring, label: "Spring" },
+      { icon: TechLogos.TypeScript, label: "TypeScript" },
+      { icon: TechLogos.Expressjs, label: "ExpressJs" },
+      { icon: TechLogos.MySQL, label: "MySQL" },
+      { icon: TechLogos.PostgreSQL, label: "PostgreSQL" },
+      { icon: TechLogos.Php, label: "PHP" },
     ],
   },
   {
     title: "Cliente",
     modifier: "client",
     skills: [
-      { icon: SkillLogos.JavaScript, label: "JavaScript" },
-      { icon: SkillLogos.React, label: "React" },
-      { icon: SkillLogos.Threejs, label: "ThreeJs" },
+      { icon: TechLogos.JavaScript, label: "JavaScript" },
+      { icon: TechLogos.React, label: "React" },
+      { icon: TechLogos.Threejs, label: "ThreeJs" },
     ],
   },
   {
     title: "Herramientas",
     modifier: "tools",
     skills: [
-      { icon: ToolLogos.Git, label: "Git" },
-      { icon: ToolLogos.Docker, label: "Docker" },
-      { icon: ToolLogos.Postman, label: "Postman" },
-      { icon: ToolLogos.Nodejs, label: "NodeJs" },
-      { icon: ToolLogos.Vite, label: "Vite" },
-      { icon: ToolLogos.WordPress, label: "Wordpress" },
+      { icon: TechLogos.Git, label: "Git" },
+      { icon: TechLogos.Docker, label: "Docker" },
+      { icon: TechLogos.Postman, label: "Postman" },
+      { icon: TechLogos.Nodejs, label: "NodeJs" },
+      { icon: TechLogos.Vite, label: "Vite" },
+      { icon: TechLogos.WordPress, label: "Wordpress" },
     ],
   },
 ];
