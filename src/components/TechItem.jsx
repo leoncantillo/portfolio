7529d60a994ab3ce8bd7b19c43c5a7a3601e19f8
Leon Cantillo <leonjob13@gmail.com>
2026-01-12 11:58:09 -0500

    [UPDATE] Unknown tech icon when icon is not provided.

--- src/components/TechItem.jsx
+++ src/components/TechItem.jsx
@@ -1,9 +1,11 @@
 import '../styles/TechItem.scss';
+import { Qicon } from '../assets/TechLogos';
 
 const TechItem = ({ icon: Icon, label, className = "" }) => {
+  const IconToRender = Icon || (label ? Qicon : null);
   return (
     <div className={"tech__item " + className}>
-      <Icon />
+      <IconToRender />
       <span className="tech__label">{label}</span>
     </div>
   );
