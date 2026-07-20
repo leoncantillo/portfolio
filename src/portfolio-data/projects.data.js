e06241e5cef4ab77fb1f8d71059f6b15e4f3f656
Leon Cantillo <leonjob13@gmail.com>
2026-07-20 15:00:56 -0500

    refactor: centralize project exports with index module.

--- src/portfolio-data/projects.data.js
+++ src/portfolio-data/projects.data.js
@@ -1,10 +1,1 @@
-import * as TechLogos from '../assets/TechLogos';
-import miscelaneaEllo from './projects/miscelanea-ello';
-import reservasuno from './projects/reservasuno';
-import solarSystem3d from './projects/solar-system-3d';
-
-export const projects = [
-  miscelaneaEllo,
-  reservasuno,
-  solarSystem3d,
-];
+export { projects } from './projects';
---
+++ src/portfolio-data/projects/index.js
@@ -0,0 +1,9 @@
+import miscelaneaEllo from './miscelanea-ello';
+import reservasuno from './reservasuno';
+import solarSystem3D from './solar-system-3d';
+
+export const projects = [
+  miscelaneaEllo,
+  reservasuno,
+  solarSystem3D,
+];
